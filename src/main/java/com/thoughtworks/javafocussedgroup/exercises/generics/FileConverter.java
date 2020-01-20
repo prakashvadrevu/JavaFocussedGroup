8946c2a6a3c71abebfae026158ed699b0688d4b4
Prakash Vadrevu <prakash.vadrevu@hotmail.com>
2020-01-21 00:51:01 +0530

    Generics exercise solution

--- src/main/java/com/thoughtworks/javafocussedgroup/exercises/generics/FileConverter.java
+++ src/main/java/com/thoughtworks/javafocussedgroup/exercises/generics/FileConverter.java
@@ -1,28 +1,69 @@
 package com.thoughtworks.javafocussedgroup.exercises.generics;
 
+import java.util.*;
+
 /*
  * TODO: Implement this class
  */
 public class FileConverter {
 
-    /*
-     * TODO: Implement logic to add the passed in converter and store it
-     */
+    private Map<Class<? extends File>, Map<Class<? extends File>, Converter>> converters;
+
+    public FileConverter() {
+        converters = new HashMap<>();
+    }
+
     public void add(Converter converter) {
-
+        Map<Class<? extends File>, Converter> toList = converters.get(converter.from());
+        if (toList == null) {
+            toList = new HashMap<>();
+        }
+        toList.put(converter.to(), converter);
+        converters.put(converter.from(), toList);
     }
 
-    /*
-     * TODO: Implement logic to iterate through the available converters and convert the given file
-     * either using a straight converter / passing through multiple converters
-     */
+
     public <T extends File, U extends File> U convert(T from, Class<U> clazz) {
-        return null;
+        return convert(from, clazz, new HashSet<>());
     }
 
-    /*
-     * TODO: Throw this exception when no Converters for the given input / output combination are available
-     */
+    public <T extends File, U extends File> U convert(T from, Class<U> clazz, Set<Class> usedConverters) {
+        if (!converters.containsKey(from.getClass())) { throw new NoConverterFoundException(); }
+        U output = convertFile(from, clazz, usedConverters);
+
+        if (output == null) {
+            Map<Class<? extends File>, Converter> convertersAvailableForSourceFile = converters.get(from.getClass());
+            for (Map.Entry<Class<? extends File>, Converter> entry : convertersAvailableForSourceFile.entrySet()) {
+                if (usedConverters.contains(from.getClass())) {
+                    continue;
+                }
+                usedConverters.add(entry.getKey());
+                File file = entry.getValue().convert(from);
+                U convertedFile = convert(file, clazz, usedConverters);
+                if (convertedFile == null) {
+                    continue;
+                }
+                return convertedFile;
+            }
+            throw new NoConverterFoundException();
+
+        } else {
+            return output;
+
+        }
+    }
+
+    private <T extends File, U extends File> U convertFile(File from, Class<U> clazz, Set<Class> usedConverters) {
+        Map<Class<? extends File>, Converter> c = converters.get(from.getClass());
+        if (c == null) { return null; }
+        Converter converter = c.get(clazz);
+        usedConverters.add(from.getClass());
+        if (converter == null || usedConverters.contains(converter)) {
+            return null;
+        }
+        return (U) converter.convert(from);
+    }
+
     public static class NoConverterFoundException extends RuntimeException {}
 
 }
